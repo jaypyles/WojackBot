8e88891a550db0025446fa7a735e0e578f564938
jaypyles <jpylesbusiness@gmail.com>
2023-05-18 22:10:57 -0500

    feat: logging to the pytest

--- tests/test_gpt.py
+++ tests/test_gpt.py
@@ -1,6 +1,11 @@
+# STL
+import logging
+
 # PDM
 import gpt4free
 from gpt4free import Provider
+
+LOG = logging.getLogger(__name__)
 
 
 def test_gpt():
@@ -8,4 +13,5 @@ def test_gpt():
     response = gpt4free.Completion.create(
         Provider.You, prompt="Write a poem on Lionel Messi"
     )
+    LOG.info("Response: %s", response)
     assert response != "Unable to fetch the response, Please try again."

f182813a6ddc3cfde9189c4623f906475c818d6c
jaypyles <jpylesbusiness@gmail.com>
2023-06-17 15:28:07 -0500

    chore: add pytest to gpt's test

--- tests/test_gpt.py
+++ tests/test_gpt.py
@@ -2,12 +2,13 @@
 import logging
 
 # PDM
+import pytest
 import gpt4free
 from gpt4free import Provider
 
 LOG = logging.getLogger(__name__)
 
-
+@pytest.mark.gpt
 def test_gpt():
     response = gpt4free.Completion.create(
         Provider.You, prompt="Write a poem on Lionel Messi"
